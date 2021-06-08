fea9a65f9c7305de26e8e13e548143ce82c0a438
mraaru <anuragpandey123buy@gmail.com>
2021-06-08 23:11:30 +0530

    hello world

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <h1>hello world!</h1>,
   document.getElementById('root')
 );
 
